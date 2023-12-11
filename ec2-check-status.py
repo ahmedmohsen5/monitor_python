64ba7ffb19a85fbffeb58edb753e6f8298ffae52
ahmed5555555555 <a.khalefa555@gmail.com>
2023-12-11 11:56:29 +0200

    frist demo in frist project

--- ec2-check-status.py
+++ ec2-check-status.py
@@ -1,1 +1,12 @@
-im
+import boto3
+ec2_client = boto3.client('ec2' , region_name= "us-east-1")
+ec2_resources = boto3.resource('ec2' , region_name= "us-east-1")
+
+
+reservations = ec2_client.describe_instances()
+for reservation in reservations['Reservations']:
+    instances = reservation["Instances"]
+    for instance in instances:
+        print(instance["ImageId"])
+        print(instance["Monitoring"])
+
